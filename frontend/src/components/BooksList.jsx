14233c3caeb0f1d1d28880daed6ccf75d940b299
Jazaltron <jazaltron10@gmail.com>
2024-08-25 23:20:32 +0100

    README update

--- frontend/src/components/BooksList.jsx
+++ frontend/src/components/BooksList.jsx
@@ -2,48 +2,48 @@
 import styled from 'styled-components';
 
 export const MainContainer = styled.section`
-width: 75vw;
-height: 75vh;
-border: 2px groove lime;
-display: flex;
-justify-content:center;
-align-items:center;
-margin: auto;  
-border-radius: 1.5rem;
-padding:.5rem;
+    width: 75vw;
+    height: 75vh;
+    border: 2px groove lime;
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    margin: auto;  
+    border-radius: 1.5rem;
+    padding:.5rem;
 `;
 export const Wrapper = styled.article`
-width: 100%;
-height: 100%;
->h2{
-  text-align:center;
-}
+    width: 100%;
+    height: 100%;
+    >h2{
+      text-align:center;
+    }
 `;
 export const Wrap = styled.div`
-display:flex;
-justify-content: space-evenly;
-align-items:center;
-padding:.5rem;
+    display:flex;
+    justify-content: space-evenly;
+    align-items:center;
+    padding:.5rem;
 `;
 export const BooksListContainer = styled.div`
-  padding:1rem;
-  border: 1px solid lime;
-  background:salmon;
+    padding:1rem;
+    border: 1px solid lime;
+    background:salmon;
   `;
   
   export const BookListItem = styled.li`
-  padding: 10px;
-  margin: 5px 0;
-  border: 1px solid lime;
-  border-radius: 5px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-right: 35px;
+    padding: 10px;
+    margin: 5px 0;
+    border: 1px solid lime;
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-right: 35px;
 
-  button {
-    margin-left: 10px;
-  }
+    button {
+      margin-left: 10px;
+    }
 `;
 
 
